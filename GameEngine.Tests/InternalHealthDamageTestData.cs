7c13b909f6e61f90a84d3c1d12ea6a24def25ec0
Qing Ma <gsmofgsm@hotmail.com>
2020-11-15 10:31:49 +0100

    sharing Test Data across tests using MemberData

---
+++ GameEngine.Tests/InternalHealthDamageTestData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameEngine.Tests
+{
+    public class InternalHealthDamageTestData
+    {
+        //private static readonly List<object[]> Data = new List<object[]>
+        //    {
+        //        new object[] {0, 100},
+        //        new object[] {1, 99},
+        //        new object[] {50, 50},
+        //        new object[] {101, 1}
+        //    };
+
+        //public static IEnumerable<object[]> TestData => Data;
+
+        // Xunit can read either from a property, a field
+        // or the return value of a method
+
+        public static IEnumerable<object[]> TestData
+        {
+            get
+            {
+                yield return new object[] { 0, 100 };
+                yield return new object[] { 1, 99 };
+                yield return new object[] { 50, 50 };
+                yield return new object[] { 101, 1 };
+            }
+        }
+    }
+}
--- GameEngine.Tests/NonPlayerCharacterShould.cs
+++ GameEngine.Tests/NonPlayerCharacterShould.cs
@@ -5,10 +5,8 @@ namespace GameEngine.Tests
     public class NonPlayerCharacterShould
     {
         [Theory]
-        [InlineData(0, 100)]
-        [InlineData(1, 99)]
-        [InlineData(50, 50)]
-        [InlineData(101, 1)]
+        [MemberData(nameof(InternalHealthDamageTestData.TestData),
+            MemberType = typeof(InternalHealthDamageTestData))]
         public void TakeDamage(int damage, int expectedHealth)
         {
             NonPlayerCharacter sut = new NonPlayerCharacter();
